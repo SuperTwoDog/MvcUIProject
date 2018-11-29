cbdde93364a3bcb399e29cfa2a8604d4be8c9edf
SuperTwoDog <986714163@qq.com>
2018-11-29 11:37:59 +0800

    第一次提交

---
+++ TSJL.Project.Web/Models/ChannelInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TSJL.Project.Web.Models
+{
+    public class ChannelInfo
+    {
+        public virtual int ID { get; set; }
+
+        public virtual string ChannelStyle { get; set; }
+
+        public virtual string ChannelCode { get; set; }
+        public virtual string CnName { get; set; }
+        public virtual string EnName { get; set; }
+
+        public virtual string Status { get; set; }
+    }
+}
